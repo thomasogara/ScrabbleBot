84f5446caf0331b170603aa8e479a0a268316823
Jarrett <jarrettpierse@gmail.com>
2020-02-10 10:41:08 +0000

    pushing empty Main class, to be used as entry-point for executable jar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         Pool pool = new Pool();
+        Player player1 = new Player("jarrettpierse");
+        Player player2 = new Player();
+        System.out.println("blah");
+
+        System.out.println(player1.displayUsername());
+        System.out.println("");
+
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,69 @@
+/*
+  Code Authors:
+  Thomas O'Gara (18379576) (thomas.ogara@ucdconnect.ie)
+  Jarrett (18375813) (jarrett.pierse@ucdconnect.ie)
+  Daniel (17481174) (daniel.nwabueze@ucdconnect.ie)
+ */
+
+import java.util.Scanner;
+
+public class Player {
+    // instance variables for username, score and player's frame (tiles)
+    private String username = "";
+    private int score = 0; // holds score for user
+    private Frame frame;
+
+
+    // default constructor for Player object
+    public Player() {
+        this.username = username;
+        this.score = score;
+        this.frame = new Frame();
+    }
+
+    // constructor with just name arg
+    public Player(String name) {
+        this.username = name;
+        this.score = score;
+        this.frame = new Frame();
+    }
+
+    // scanner object to take in username input
+    public static final Scanner users = new Scanner(System.in); // takes username input
+
+    // reset player data method
+    public void resetUser(){
+       this.username = "";
+       this.score = 0;
+    }
+
+    // set player's name method
+    public void setUsername(String name){
+        this.username = name;
+    }
+
+    // set user score
+    public void setScore(int value){
+       this.score = value;
+    }
+
+    // increase score method - takes an int as argument as adds it to the player's score
+    public void increaseScore(int a) {
+        this.score = this.score + a;
+    }
+
+    // access player score
+    public int getScore() {
+        return this.score;
+    }
+
+    // access players tiles
+    public Character[] getFrame() {
+        return this.frame.getLettersAsCharacterArray();
+    }
+
+    // displays username when called
+    public String displayUsername() {
+        return this.username;
+    }
+}
